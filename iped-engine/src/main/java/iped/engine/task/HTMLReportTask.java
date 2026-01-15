96704487b67e775fa3cae003832374864a255862
gfd2020 <59742865+gfd2020@users.noreply.github.com>
2026-01-15 11:23:18 -0300

    fix report name again

--- iped-engine/src/main/java/iped/engine/task/HTMLReportTask.java
+++ iped-engine/src/main/java/iped/engine/task/HTMLReportTask.java
@@ -396,7 +396,7 @@ public class HTMLReportTask extends AbstractTask {
         File pdf_cmds = File.createTempFile("pdf_cmds", ".tmp");
         pdf_cmds.deleteOnExit();
 
-        File pdfTempOutput = File.createTempFile(Messages.getString("HTMLReportTask.ReportFileName").replace(".htm",""), ".pdf");
+        File pdfTempOutput = File.createTempFile("report", ".pdf");
         pdfTempOutput.deleteOnExit();
 
         List<String> arguments = new ArrayList<String>();
@@ -511,7 +511,7 @@ public class HTMLReportTask extends AbstractTask {
             if (readThread != null)
                 readThread.interrupt();
 
-            copyFile(pdfTempOutput,outputFolder,"report.pdf");
+            copyFile(pdfTempOutput,outputFolder,Messages.getString("HTMLReportTask.ReportFileName").replace(".htm",".pdf"));
 
         }
 
